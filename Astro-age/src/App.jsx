70bc14fcc889e7d4292f174d37097fc73fc674be
anasMoukhtar <anasmoukhtar321@gmail.com>
2025-05-16 20:20:01 +0300

    added orbits

--- Astro-age/src/App.jsx
+++ Astro-age/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+import { useState } from 'react'
+import Planets from './components/Planets'
+const App = ()=>{
+const [age , setAge] = useState(0)
+const [planet , setPlanet] = useState('Earth')
+return (
+  <div>
+    <div className="get_age">
+      <label htmlFor="age">Age</label>
+      <input type="number" id="age" onChange={(e)=>setAge(e.target.value)}/>
+    </div>
+    <label>Select a planet</label>
+    <Planets setPlanet={setPlanet}/>
+    <div className="age">
+      {age && <p>Your Age at {planet} is {age}</p>}
+    </div>
+  </div>
+)
 }
-
 export default App
